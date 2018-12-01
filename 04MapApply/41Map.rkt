4529e4ae2a0b1bd070d5f94b364f9e85991188c2
leprekon91 <reist2009@gmail.com>
2018-12-02 00:24:21 +0200

    Add Another Map Example

--- 04MapApply/41Map.rkt
+++ 04MapApply/41Map.rkt
@@ -7,5 +7,14 @@
         (lambda(x y) (+ x y))       ; function 
         (list 1 2 3) (list 4 5 6)   ; lists
         )
-)
+    )
 ;expected output: (5 7 9)
+(display "\n")
+;Another Example
+(display
+    (map
+        (lambda(x y z) (list x y z))
+        (list 1 2 3 4 5) (list 2 3 4 5 6) (list 3 4 5 6 7)
+        )
+    )
+;expected output: ((1 2 3) (2 3 4) (3 4 5) (4 5 6) (5 6 7))
