2928755fc480fc699e4588da5a5be680ebd855cf
leprekon91 <reist2009@gmail.com>
2018-12-01 17:33:09 +0200

    Add more functions examples  to file 33.

--- 03Lists/33Other.rkt
+++ 03Lists/33Other.rkt
@@ -13,3 +13,24 @@
 ;[f(0), f(1), f(2), ..., f(n-1)] 
 (define dec (lambda(x) (- x 1)))
 (display (build-list 10 dec))
+(display "\n")
+; length will return length of list
+(display (length list_demo))
+(display "\n")
+
+;append will take a list of lists and returns a concatated list of them
+(define list_demo2 (list #\a #\b #\c))
+(display (append list_demo list_demo2))
+(display "\n")
+
+(display list_demo)
+(display "\n")
+
+;list-ref receives a list 'lst' and a positive integer 'i',and returns lst[i]
+(display (list-ref list_demo 1))
+(display "\n")
+
+
+;list-tail will return the list tail after the specified index
+(display (list-tail list_demo 1))
+(display "\n")
