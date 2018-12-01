3a50ef2937998c60697c53c16475e5fba2c734a1
leprekon91 <reist2009@gmail.com>
2018-12-01 11:26:53 +0200

    Add 31 - list basic functions file

---
+++ 03Lists/31Definitions.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(define lst ( list 1 2 3 )) ; list function returns all arguments as part of a single list
+    (display "\n")
+( display (car lst) )       ; 'car' returns the head of the list
+    (display "\n")
+( display (cdr lst) )       ; 'cdr' returns a list without it's head
+    (display "\n")
+( display (cons 4 lst))     ; 'cons' returns a list with a new head
+    (display "\n")
+( display (null? lst))      ; 'null?' returns true if the list is empty
+    (display "\n")
