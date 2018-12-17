fe48c8638d68c95e8d7711245314eac318cd279a
Andrey Grabarnick <andreyg@exypnos.co.il>
2018-12-17 08:14:49 +0200

    Add question 13 and 14 files

---
+++ questions/q13.rkt
@@ -0,0 +1,3 @@
+#lang racket
+; Question 13 - Write a function which will receive a list,
+; and returns all of this lists permutations.
---
+++ questions/q14.rkt
@@ -0,0 +1,2 @@
+#lang racket
+; Question 14 - Write a function that receives one argument and returns a list of functions 
